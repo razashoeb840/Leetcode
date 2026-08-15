83b5278473366a4f8061bfddbc9c1f430659abc0
SHOEB RAZA <razashoeb3051@gmail.com>
2026-08-16 02:16:22 +0530

    Time: 0 ms (100%), Space: 25.7 MB (58.71%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.cpp
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.cpp
@@ -1,17 +1,35 @@
+// class Solution {
+// public:
+//     double minimumAverage(vector<int>& v) {
+//         double ans=INT_MAX;
+//         int n=v.size()/2;
+
+//         for(int i=0;i<n;i++){
+//             int mx=*max_element(v.begin(),v.end());
+//             int mn=*min_element(v.begin(),v.end());
+
+//             ans=min(ans,(mx+mn)/2.0);
+
+//             v.erase(find(v.begin(),v.end(),mx));
+//             v.erase(find(v.begin(),v.end(),mn));
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     double minimumAverage(vector<int>& v) {
+        sort(v.begin(),v.end());
+
+        int low=0,high=v.size()-1;
         double ans=INT_MAX;
-        int n=v.size()/2;
 
-        for(int i=0;i<n;i++){
-            int mx=*max_element(v.begin(),v.end());
-            int mn=*min_element(v.begin(),v.end());
-
-            ans=min(ans,(mx+mn)/2.0);
-
-            v.erase(find(v.begin(),v.end(),mx));
-            v.erase(find(v.begin(),v.end(),mn));
+        while(low<high){
+            ans=min(ans,(v[low]+v[high])/2.0);
+            low++;
+            high--;
         }
 
         return ans;
