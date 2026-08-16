53a346944c388c90c689e0f985c34e27d678b865
SHOEB RAZA <razashoeb3051@gmail.com>
2026-08-17 00:40:48 +0530

    Time: 17 ms (5.33%), Space: 145.2 MB (6.85%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> p,n,ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<0) n.push_back(nums[i]);
+            else p.push_back(nums[i]);
+        }
+
+         for(int i=0;i<n.size();i++){
+
+            ans.push_back(p[i]);
+            ans.push_back(n[i]);
+
+         }
+
+
+        return ans;
+    }
+};
