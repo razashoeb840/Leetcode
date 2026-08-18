3661685daa76ae60ae3f35d39d87a7c493512c04
SHOEB RAZA <razashoeb3051@gmail.com>
2026-08-19 00:33:46 +0530

    Time: 21 ms (93.24%), Space: 57.4 MB (23.61%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        sort(piles.begin(),piles.end());
+        int my=piles.size()-2;
+        int ans=0;
+        for(int i=0;i<piles.size()/3;i++){
+            ans+=piles[my];
+            my-=2;
+        }
+        return ans;
+    }
+};
