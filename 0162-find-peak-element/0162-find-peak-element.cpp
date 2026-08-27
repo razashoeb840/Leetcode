e3d115393b985c84a451af62eef718e0ae8440de
SHOEB RAZA <razashoeb3051@gmail.com>
2026-08-27 19:41:17 +0530

    Time: 0 ms (100%), Space: 12.8 MB (10.53%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         if(nums.size()==1) return 0;
-        else if(nums[0]>nums[1]) return 0;
-        else if(nums[nums.size()-1]>nums[nums.size()-2]) return nums.size()-1;
-        else {
-            for(int i=1;i<nums.size()-1;i++){
-                if(nums[i]>nums[i-1] && nums[i]>nums[i+1]) return i;
-            }
+        if(nums[0]>nums[1]) return 0;
+        if(nums[nums.size()-1]>nums[nums.size()-2]) return nums.size()-1;
+        
+        for(int i=1;i<nums.size()-1;i++){
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]) return i;
+            
         }
         return -1;
     }
