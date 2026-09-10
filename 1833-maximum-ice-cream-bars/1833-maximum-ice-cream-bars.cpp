437b47831c2228f24c2e01f6dbf4a83d5197b4d3
SHOEB RAZA <razashoeb3051@gmail.com>
2026-09-10 19:41:33 +0530

    Time: 40 ms (23.99%), Space: 80.3 MB (56.89%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -3,10 +3,7 @@ public:
     int maxIceCream(vector<int>& nums, int coins) {
         sort(nums.begin(),nums.end());
         int cnt=0,sum=0,i=0;
-        // for(int i=0;i<nums.size();i++){
-        //     sum+=nums[i];
-        //     if(sum<coins) cnt++;
-        // }
+       
 
         while(i<nums.size() && sum+nums[i]<=coins){
             sum+=nums[i];
