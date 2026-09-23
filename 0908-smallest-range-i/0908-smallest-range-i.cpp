e46640a8824b2da7307ac6ded54846ee30574d04
SHOEB RAZA <razashoeb3051@gmail.com>
2026-09-24 03:04:06 +0530

    Time: 0 ms (100%), Space: 19.3 MB (24.98%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.cpp
+++ 0908-smallest-range-i/0908-smallest-range-i.cpp
@@ -3,12 +3,19 @@ public:
     int smallestRangeI(vector<int>& nums, int k) {
         int mx=*max_element(nums.begin(),nums.end());
         int mn=*min_element(nums.begin(),nums.end());
-        for(int i=0;i<k;i++){
-            mx--;
-            mn++;
-            if(mn>=mx) return 0;
+        // for(int i=0;i<k;i++){
+        //     mx--;
+        //     mn++;
+        //     if(mn>=mx) return 0;
 
-        }
+        // }
+
+        // return mx-mn;
+
+        mx-=k;
+        mn+=k;
+
+        if(mn>=mx)return 0;
 
         return mx-mn;
         
